e7135deb21cd7c9bf1e7a4dcc93f9eaee2641405
Javier <jferreyra@fi.uba.ar>
2020-05-17 06:34:14 -0300

    add comment

--- TP1/vecinos.c
+++ TP1/vecinos.c
@@ -10,6 +10,13 @@ unsigned int vecinos(unsigned char *a, unsigned int i, unsigned int j, unsigned 
                 continue;
             x = (M+i+k) % M;
             y = (N+j+l) % N;
+            /*
+            * Explicacion: uso (M+i+k) y (N+j+l) en vez de solo (i+k) y (j+l) porque la operacion
+            * modulo de C funciona mal con numeros negativos (por ejemplo cuando tengo la posicion 
+            * 0,0 y quiero revisar a la izquierda me queda -1,0). Por eso le sumo M y N respectivamente
+            * asi no es negativo y el modulo da lo mismo. En stack overflow esta explicado por que el
+            * modulo da mal con numeros negativos.
+            */
             index = N*x+y;
             if (a[index] == 1)
                 n++;
