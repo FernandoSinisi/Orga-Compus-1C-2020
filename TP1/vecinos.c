c86dd1eb1194d5fd2f5a1374b928b150a0117f53
Javier <jferreyra@fi.uba.ar>
2020-05-17 06:20:54 -0300

    implement vecinos (C)

--- TP1/vecinos.c
+++ TP1/vecinos.c
@@ -1,4 +1,19 @@
+#include <stdio.h>
+
 unsigned int vecinos(unsigned char *a, unsigned int i, unsigned int j, unsigned int M, unsigned int N){
-    //TODO
-    return 2;
+    unsigned int n, x, y, index;
+    int k, l;
+    n = 0;
+    for (k = -1; k <= 1; k++){
+        for (l = -1; l <= 1; l++){
+            if ((k == 0) && (l == 0))
+                continue;
+            x = (M+i+k) % M;
+            y = (N+j+l) % N;
+            index = N*x+y;
+            if (a[index] == 1)
+                n++;
+        }
+    }
+    return n;
 }
