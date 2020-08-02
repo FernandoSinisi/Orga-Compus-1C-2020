56f7021c761826d50927c454ab02afa5e38beb78
komod0 <juliancrespo15@gmail.com>
2020-08-02 04:24:12 -0300

    Jrr para datapath pipeline, hay que probarlo

---
+++ TP3/prueba-jr-pipeline.asm
@@ -0,0 +1,24 @@
+li $t7, 0
+li $t5, 60
+li $t6, 6
+addi $t2, $t0, 2
+andi $t1,$t2,3
+jrr $t0, $t5
+li $t3, 2
+hola:
+li $t4, 6
+li $t0, 5
+li $t0, 5
+li $t0, 5
+li $t0, 5
+li $t0, 5
+li $t0, 5
+li $t0, 5
+li $t0, 5
+li $t0, 5
+li $t0, 5
+li $t0, 5
+li $t0, 5
+li $t0, 5
+li $t0, 5
+
